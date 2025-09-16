6c0e1ecef7179e42c4a0d1c74798635382f81238
ajh9498 <ajh9498@hansolo.poly.edu>
2025-09-16 14:33:30 -0400

    updated source code with versions for single conv. and full signal filtering

--- FIR/src/fir.h
+++ FIR/src/fir.h
@@ -3,7 +3,7 @@
 #include "mc_scverify.h"
 
 #ifndef TYPE_T
-#define TYPE_T my_float_t
+#define TYPE_T int
 #endif
 typedef TYPE_T type_t;
 
@@ -11,8 +11,12 @@ typedef TYPE_T type_t;
 #define NUM_TAPS 16
 #endif
 
-type_t CCS_BLOCK(fir)(type_t input, type_t taps[NUM_TAPS]);
+/* VERSION A: a single convolution of the filter */
+// type_t CCS_BLOCK(fir)(type_t input, type_t taps[NUM_TAPS]);
 
-#define NUM_SAMPLES 16
+/* VERSION B: a streaming filter processing NUM_SAMPLES samples */
+void CCS_BLOCK(fir)(ac_channel<type_t> &in_channel, ac_channel<type_t> &out_channel, type_t taps[NUM_TAPS]);
 
-void test(ac_channel<type_t> &in_channel, ac_channel<type_t> &out_channel, type_t taps[NUM_TAPS]);
+#define NUM_SAMPLES 1024
+
+void CCS_BLOCK(test)(ac_channel<type_t> &in_channel, ac_channel<type_t> &out_channel, type_t taps[NUM_TAPS]);
--- FIR/src/main.cpp
+++ FIR/src/main.cpp
@@ -8,8 +8,7 @@ CCS_MAIN(int argc, char *argv[]) {
     type_t taps[NUM_TAPS] = {-37, -90, -115, -85, 12,  152,  315, 470,
                              470, 315, 152,  12,  -85, -115, -90, -37};
 
-    type_t signal[NUM_SAMPLES] = {1, 0, 0, 0, 0, 0, 0, 0,
-                                  0, 0, 0, 0, 0, 0, 0, 0};
+    type_t signal[NUM_SAMPLES] = {1};
 
     for (int i = 0; i < NUM_SAMPLES; i++) {
         in_channel.write(signal[i]);
@@ -17,12 +16,11 @@ CCS_MAIN(int argc, char *argv[]) {
 
     test(in_channel, out_channel, taps);
 
-    // for (int i = 0; i < NUM_SAMPLES; i++) {
-    //     if (!out_channel.empty()) {
-    //         printf("Output[%d] = %d\n", i, out_channel.read().to_int()); //
-    //         NOTE: need to use conversion method for correct output
-    //     }
-    // }
+    for (int i = 0; i < NUM_SAMPLES; i++) {
+        if (!out_channel.empty()) {
+            printf("Output[%d] = %d\n", i, out_channel.read()); // NOTE: need to use conversion method for correct output
+        }
+    }
 
     CCS_RETURN(0);
 }
