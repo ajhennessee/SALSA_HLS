8e123346d5d7a9c4709aa5a8ed667c217a8d9b6e
ajh9498 <ajh9498@hansolo.poly.edu>
2025-09-30 14:08:39 -0400

    updates to FIR code

--- FIR/src/fir.h
+++ FIR/src/fir.h
@@ -1,21 +1,21 @@
 #include "types.h"
-#include "ac_channel.h"
-#include "mc_scverify.h"
+#include <ac_channel.h>
+#include <mc_scverify.h>
 
 #ifndef TYPE_T
-#define TYPE_T int
+#define TYPE_T my_float_t
 #endif
 typedef TYPE_T type_t;
 
 #ifndef NUM_TAPS
-#define NUM_TAPS 16
+#define NUM_TAPS 32
 #endif
 
 /* VERSION A: a single convolution of the filter */
-// type_t CCS_BLOCK(fir)(type_t input, type_t taps[NUM_TAPS]);
+type_t CCS_BLOCK(fir)(type_t input, type_t taps[NUM_TAPS]);
 
 /* VERSION B: a streaming filter processing NUM_SAMPLES samples */
-void CCS_BLOCK(fir)(ac_channel<type_t> &in_channel, ac_channel<type_t> &out_channel, type_t taps[NUM_TAPS]);
+// void CCS_BLOCK(fir)(ac_channel<type_t> &in_channel, ac_channel<type_t> &out_channel, type_t taps[NUM_TAPS]);
 
 #define NUM_SAMPLES 1024
 
