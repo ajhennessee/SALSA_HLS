3c4af652f84696c133cf1849d831e72516b6384b
ajh9498 <ajh9498@hansolo.poly.edu>
2025-09-30 15:25:24 -0400

    minor updates to source code

--- mat_mul/src/mat_mul.cpp
+++ mat_mul/src/mat_mul.cpp
@@ -1,16 +1,16 @@
 #include "mat_mul.h"
 
-void matrix_multiply(type_t A[N][M], type_t B[M][P], type_t C[N][P]) {
+void matrix_multiply(type_t A[Nd][Md], type_t B[Md][Pd], type_t C[Nd][Pd]) {
     
-    ROW: for (int i = 0; i < N; i++) {
+    ROW: for (int i = 0; i < Nd; i++) {
         
-        COL: for (int j = 0; j < P; j++) {
+        COL: for (int j = 0; j < Pd; j++) {
             
             type_t sum = (type_t)0;
             
-            MUL: for (int k = 0; k < M; k++) {
-                // sum += A[i][k] * B[k][j]; // for my_int_t
-                sum.add(sum, (type_t)(A[i][k] * B[k][j])); // for my_float_t
+            MUL: for (int k = 0; k < Md; k++) {
+                sum += A[i][k] * B[k][j]; // for my_int_t
+                // sum.add(sum, (type_t)(A[i][k] * B[k][j])); // for my_float_t
                 // sum = sum + (type_t)(A[i][k] * B[k][j]); // for my_complex_float_t
             }
             
--- mat_mul/src/mat_mul.h
+++ mat_mul/src/mat_mul.h
@@ -1,17 +1,17 @@
 #include "types.h"
 
-#ifndef N
-#define N 1024 // Rows of A, Rows of C
+#ifndef Nd
+#define Nd 1024 // Rows of A, Rows of C
 #endif
 
-#ifndef M
-#define M 4 // Columns of A, Rows of B
+#ifndef Md
+#define Md 4 // Columns of A, Rows of B
 #endif
 
-#ifndef P
-#define P 8 // Columns of B, Columns of C
+#ifndef Pd
+#define Pd 8 // Columns of B, Columns of C
 #endif
 
 typedef my_float_t type_t;
 
-void matrix_multiply(type_t A[N][M], type_t B[M][P], type_t C[N][P]);
+void matrix_multiply(type_t A[Nd][Md], type_t B[Md][Pd], type_t C[Nd][Pd]);
